e0259801b5a01c1e45219e2592b23fe51f2c03a1
Reinaldo Camargo <camargo.reinaldo@hotmail.com>
2024-04-25 17:08:53 -0300

    Created the search view layout

--- GithubSearchAppUIKit GithubSearchAppUIKit/FavoritesListViewController.swift
+++ GithubSearchAppUIKit GithubSearchAppUIKit/FavoritesListViewController.swift
@@ -12,6 +12,11 @@ class FavoritesListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemBlue
+        view.backgroundColor = .systemBackground
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
     }
 }
---
+++ GithubSearchAppUIKit GithubSearchAppUIKit/GFButton.swift
@@ -0,0 +1,38 @@
+//
+//  GFButton.swift
+//  GithubSearchAppUIKit GithubSearchAppUIKit
+//
+//  Created by Reinaldo Camargo on 25/04/24.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        //custom code
+        configure()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+
+}
