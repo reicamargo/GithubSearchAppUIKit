ad753ee4607984f012ccb5530a5f743fd679d09e
Reinaldo Camargo <camargo.reinaldo@hotmail.com>
2024-04-30 15:18:35 -0300

    Added favorites cell, tableView and delete favorite

--- GithubSearchAppUIKit/Screens/FavoritesListViewController.swift
+++ GithubSearchAppUIKit/Screens/FavoritesListViewController.swift
@@ -10,28 +10,51 @@ import UIKit
 class FavoritesListViewController: UIViewController {
 
     private var favorites: [Follower] = []
+    private let tableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .systemBackground
+        
+        configureViewController()
+        configureTableView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        navigationController?.isNavigationBarHidden = true
         
         Task {
             await loadFavorites()
         }
     }
     
+    private func configureViewController() {
+        view.backgroundColor = .systemBackground
+        title = "Favorites"
+    }
+    
+    private func configureTableView() {
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        tableView.rowHeight = 80
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(FavoriteCell.self, forCellReuseIdentifier: FavoriteCell.reuseID)
+    }
+    
     private func loadFavorites() async {
         showLoadingView()
         
         do {
-            self.favorites = try await PersistenceManager.retrieveFavorites()
-            print( self.favorites)
+            let favorites = try await PersistenceManager.retrieveFavorites()
+            
+            if favorites.isEmpty {
+                showEmptyStateView(with: "No favorites?\nAdd one on the follower screen", in: self.view)
+            } else {
+                self.favorites = favorites
+                self.tableView.reloadData()
+                self.view.bringSubviewToFront(self.tableView)
+            }
+            
         } catch {
             var alertItem: AlertItem
             
@@ -43,6 +66,54 @@ class FavoritesListViewController: UIViewController {
         }
         
         dismissLoadingView()
+    }
+}
+
+extension FavoritesListViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favorites.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: FavoriteCell.reuseID) as! FavoriteCell
+        let favorite = favorites[indexPath.row]
+        cell.set(favorite: favorite)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let favorite = favorites[indexPath.row]
+        let destVC = FollowerListViewController()
+        destVC.username = favorite.login
+        destVC.title = favorite.login
+        
+        navigationController?.pushViewController(destVC, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else { return }
+        
+        let favorite = favorites[indexPath.row]
+        
+        favorites.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .left)
+        
+        Task {
+            do {
+                try await PersistenceManager.update(with: favorite, actionType: .remove)
+            } catch {
+                var alertItem: AlertItem
+                
+                if let persistenceError = error as? PersistenceError {
+                    alertItem = AlertItem(title: "Something is wrong", message: persistenceError.rawValue)
+                } else { alertItem = AlertItemContext.defaultError }
+                
+                self.presentGFAlertOnMainThread(title: alertItem.title, message: alertItem.message, buttonTitle: "Ok")
+            }
+        }
+        
         
     }
+    
 }
---
+++ GithubSearchAppUIKit/Views/Cells/FavoriteCell.swift
@@ -0,0 +1,51 @@
+//
+//  FavoriteCell.swift
+//  GithubSearchAppUIKit
+//
+//  Created by Reinaldo Camargo on 30/04/24.
+//
+
+import UIKit
+
+class FavoriteCell: UITableViewCell {
+    static let reuseID = "FavoriteCell"
+    private let avatarImageView = GFAvatarImageView(frame: .zero)
+    private let usernameLabel = GFTitleLabel(textAlignment: .left, fontSize: 26)
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configure()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set(favorite: Follower) {
+        usernameLabel.text = favorite.login
+        Task {
+            await avatarImageView.loadImage(from: favorite.avatarUrl)
+        }
+    }
+    
+    private func configure() {
+        addSubview(avatarImageView)
+        addSubview(usernameLabel)
+        
+        accessoryType = .disclosureIndicator
+        let padding: CGFloat = 12
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            avatarImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: padding),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 60),
+            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor),
+            
+            usernameLabel.centerYAnchor.constraint(equalTo: avatarImageView.centerYAnchor),
+            usernameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 24),
+            usernameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 30)
+        ])
+    }
+}
